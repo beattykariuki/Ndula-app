58e67ab8b05fd14099ebabf56fcf7b8295771725
beattykariuki <bettykariuki026@gmail.com>
2019-03-27 17:52:22 +0300

    created new files

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/beatty/Desktop/projects/Django/ndula-advert/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
